3ecb52e40545926480db5b1c0f42df968136157d
jacob-plumb <cied@fresnou.org>
2017-04-05 22:36:05 -0700

    Progress on stat allocation

--- script.js
+++ script.js
@@ -16,6 +16,8 @@
   //Math.random();
   //parseInt();
 
+  var pointPool = 4;
+
   var player = {
     str: 5,
     dex: 5,
@@ -39,23 +41,16 @@
     return count;
   }
 
-  function addStat(stat) {
-    stat += 1;
-    return stat;
-  }
 
-  function subtractStat(stat) {
-    stat -= 1;
-    return stat;
-  }
-
-  console.log(addStat(4))
   console.log(rollDice(3, 6, 1));
 
 
   $(document).ready(function() {
 
+    $("#points").text(pointPool);
     $("#str").text(player.str);
+    $("#dex").text(player.dex);
+    $("#con").text(player.dex);
 
     $("#buttonD20").click(function() {
       //alert("Handler for .click() called.");
@@ -66,16 +61,58 @@
       $("#roll6").text(rollDice(1,6,0));
     });
 
-    $("#buttonAddStrength").click(function() {
-      var $str = $("#str");
-      var value = parseInt($str.text());
-      $("#str").text(addStat(value));
+    $("#buttonAddStr").click(function() {
+      if (player.str + 1 < 11 && pointPool > 0) {
+        player.str += 1;
+        pointPool -= 1;
+        $("#str").text(player.str);
+        $("#points").text(pointPool);
+      }
     });
 
-    $("#buttonSubtractStrength").click(function() {
-      var $str = $("#str");
-      var value = parseInt($str.text());
-      $("#str").text(subtractStat(value));
+    $("#buttonSubtractStr").click(function() {
+      if (player.str - 1 > 0) {
+        player.str -= 1;
+        pointPool += 1;
+        $("#str").text(player.str);
+        $("#points").text(pointPool);
+      }
+    });
+
+    $("#buttonAddDex").click(function() {
+      if (player.dex + 1 < 11 && pointPool > 0) {
+        player.dex += 1;
+        pointPool -= 1;
+        $("#dex").text(player.dex);
+        $("#points").text(pointPool);
+      }
+    });
+
+    $("#buttonSubtractDex").click(function() {
+      if (player.dex - 1 > 0) {
+        player.dex -= 1;
+        pointPool += 1;
+        $("#dex").text(player.dex);
+        $("#points").text(pointPool);
+      }
+    });
+
+    $("#buttonAddCon").click(function() {
+      if (player.con + 1 < 11 && pointPool > 0) {
+        player.con += 1;
+        pointPool -= 1;
+        $("#con").text(player.con);
+        $("#points").text(pointPool);
+      }
+    });
+
+    $("#buttonSubtractCon").click(function() {
+      if (player.con - 1 > 0) {
+        player.con -= 1;
+        pointPool += 1;
+        $("#con").text(player.con);
+        $("#points").text(pointPool);
+      }
     });
 
 
